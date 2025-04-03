5279cceb664b81cf2a8c2cee4cd146757f31e455
Mmildcws <122968161+Mmildcws@users.noreply.github.com>
2025-04-03 19:56:11 +0700

    4 sports finish

--- SportUI/src/App.jsx
+++ SportUI/src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   const [distance, setDistance] = useState("0");
   const [weight, setWeight] = useState("0");
   const [ans, setAns] = useState(0);
+  const [time, setTime] = useState("0");
+  const [sportType, setSportType] = useState('RUN');
   const[Save] = useState("0");
 
   const handleSave = () => {
@@ -16,28 +18,52 @@ function App() {
   };
 const OK = () => 
 {
-  setAns(weight*distance*1.036) 
+  if (sportType === 'RUN') {
+    setAns(weight * distance * 1.036);
+  } else if (sportType === 'CYCLING') {
+    setAns(time * 450); 
+  } else if (sportType === 'BADMINTON'){
+    setAns(time *350);
+  } else if (sportType === 'ZUMBA') {
+    setAns(time *500);
+  }
+    
 };
 
   return (
     <>
     <h1>sport</h1>
     <div className="run">
-      <button className="run-button" onClick={() => setIsOpen(true)}>RUN</button>
+      <button className="run-button" onClick={() => {setSportType('RUN'); setIsOpen(true)}}>RUN</button>
+    </div>
+
+    <div className='cycling'>
+      <button className='cycling-button' onClick={() => { setSportType('CYCLING'); setIsOpen(true); }}>CYCLING</button>
+    </div>
+
+    <div className='badminton'>
+      <button className='badminton-button' onClick={() => { setSportType('BADMINTON'); setIsOpen(true); }}>BADMINTON</button>
+    </div>
+
+    <div className='zumba'>
+      <button className='zumba-button' onClick={() => { setSportType('ZUMBA'); setIsOpen(true); }}>ZUMBA</button>
     </div>
 
     <ModalBox
-    isOpen = {isOpen} 
-    onClose = {() => setIsOpen(false)}
-    distance = {distance} 
-    setDistance = {setDistance} 
-    weight = {weight}
-    setWeight = {setWeight}
-    Save = {handleSave}
-    ans={ans}
-    OK={OK}
+      isOpen = {isOpen} 
+      onClose = {() => { setIsOpen(false); setAns(0); setTime(0); setDistance(0); setWeight(0);}}
+      distance = {distance} 
+      setDistance = {setDistance} 
+      weight = {weight}
+      setWeight = {setWeight}
+      Save = {handleSave}
+      ans={ans}
+      OK={OK}
+      time={time}
+      setTime={setTime}
+      sportType={sportType}/>
 
-    />
+    
     
     </>
   )
--- SportUI/src/ModalBox.jsx
+++ SportUI/src/ModalBox.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import "./ModalBox.css";
 
-const ModalBox = ({ isOpen, onClose, distance, setDistance, weight, setWeight, ans, OK, Save  }) => {
+import runImage from './run.png'
+import cyclingImage from './cycling.png'
+import badmintonImage from './badminton.png' 
+import zumbaImage from './zumba.png' 
+
+const ModalBox = ({ isOpen, onClose, distance, setDistance, weight, setWeight, ans, OK, Save,sportType,time,setTime  }) => {
     if (!isOpen) return null;
 
 // const [distance, setDistance] = useState(0);
@@ -11,8 +16,12 @@ const ModalBox = ({ isOpen, onClose, distance, setDistance, weight, setWeight, a
 return(
     <div className="modal">
         <span className="close" onClick={onClose}>&times;</span>
-        <h1>RUN</h1>
+        <h1>{sportType}</h1> 
+
         <div className="modal-input">
+        {sportType === 'RUN' && (
+            <>
+            <img src={runImage}/>
             <label>distance(km):</label>
             <input type="number"
             value={distance}
@@ -22,6 +31,41 @@ return(
             <input type="number"
             value={weight}
             onChange={(e) => setWeight(e.target.value)} />
+            </>
+        )}
+
+        {sportType === 'CYCLING' && (
+            <>
+                <img src={cyclingImage}/>
+                <label>Time(hours):</label>
+                <input
+                type="number"
+                value={time}
+                onChange={(e) => setTime(e.target.value)} />
+            </>
+        )}
+
+        {sportType === 'BADMINTON' && (
+            <>
+                <img src={badmintonImage}/>
+                <label>Time(hours):</label>
+                <input
+                type="number"
+                value={time}
+                onChange={(e) => setTime(e.target.value)} />
+            </>
+        )}
+
+        {sportType === 'ZUMBA' && (
+            <>
+                <img src={zumbaImage}/>
+                <label>Time(hours):</label>
+                <input
+                type="number"
+                value={time}
+                onChange={(e) => setTime(e.target.value)} />
+            </>
+        )}
 
             <h2>Calories Burned(cal): {ans}</h2>
         </div>
@@ -32,6 +76,7 @@ return(
         </div>
         
     </div>
+    
 );
 };
 export default ModalBox;
