e03238a1c06327159ecaf94c67715ea3ccc7e193
aekasit <aekasit2002@gmail.com>
2025-04-18 20:21:18 +0700

    feat: refactor App component to include routing and navigation for HealthDashboard and HealthCalculatorUI

--- home-screen/src/App.jsx
+++ home-screen/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Navigation from "./components/์Navigation.jsx";
 import HealthDashboard from "./components/HealthDashboard.jsx";
+import HealthCalculatorUI from "./components/HealthCalculatorUI.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <HealthDashboard />
-    </>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/HealthDashboard" element={<HealthDashboard />} />
+        <Route path="/bmi" element={<HealthCalculatorUI />} /> 
+      </Routes>
+    </Router>
   );
 }
 
