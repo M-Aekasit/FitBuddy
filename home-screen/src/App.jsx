3ace04de6d03418a0051a0f9080dc898d917145a
aekasit <aekasit2002@gmail.com>
2025-04-18 20:59:30 +0700

    feat: add Water component route to App

--- home-screen/src/App.jsx
+++ home-screen/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from "./components/์Navigation.jsx";
 import HealthDashboard from "./components/HealthDashboard.jsx";
 import HealthCalculatorUI from "./components/HealthCalculatorUI.jsx";
+import Water from "./components/Water.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,7 +15,8 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/HealthDashboard" element={<HealthDashboard />} />
-        <Route path="/bmi" element={<HealthCalculatorUI />} /> 
+        <Route path="/bmi" element={<HealthCalculatorUI />} />
+        <Route path="/water" element={<Water />} />
       </Routes>
     </Router>
   );
