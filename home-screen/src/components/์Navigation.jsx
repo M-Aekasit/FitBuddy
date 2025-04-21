b546e4ce9a3e6a5a3c915fbb74bf75b668fdf9e9
aekasit <aekasit2002@gmail.com>
2025-04-21 14:44:23 +0700

    feat: update Navigation component to include links for Friend and Diary pages

--- home-screen/src/components/์Navigation.jsx
+++ home-screen/src/components/์Navigation.jsx
@@ -4,9 +4,8 @@ import { Link } from 'react-router-dom';
 const Navigation = () => (
   <nav className="flex gap-4 p-4 bg-gray-100">
     <Link to="/HealthDashboard" className="hover:text-blue-500">Home</Link>
-    {/* <Link to="/Water" className="hover:text-blue-500">Water Tracker</Link> */}
-    {/* <Link to="/bmi" className="hover:text-blue-500">BMI Calculator</Link> */}
-
+    <Link to="/FriendPage" className="hover:text-blue-500">Friend</Link>
+    <Link to="/DiaryPage" className="hover:text-blue-500">Diary</Link>
   </nav>
 );
 
