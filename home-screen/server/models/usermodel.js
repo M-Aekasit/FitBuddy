c9f02a6c274b9a927eff48024ee9812f8c7b051c
aekasit <aekasit2002@gmail.com>
2025-05-07 23:08:59 +0700

    feat: add user model with schema for user data management

---
+++ home-screen/server/models/usermodel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, required: true, unique: true },
+    password: String,
+    age: Number,
+    gender: String,
+    weight: Number,
+    height: Number,
+    bmi: {
+      value: Number,
+      category: String,
+    },
+    bmr: Number,
+    tdee: Number,
+    activityLevel: {
+      type: String,
+      enum: [
+        "Sedentary",
+        "Lightly active",
+        "Moderately active",
+        "Very active",
+        "Super active",
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
