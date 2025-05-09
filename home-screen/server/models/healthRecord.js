416649743f8c5d8efe8262bd6777cc25cccd2945
aekasit <aekasit2002@gmail.com>
2025-05-10 04:27:34 +0700

    fix: ensure user field is required in healthRecord schema

--- home-screen/server/models/healthRecord.js
+++ home-screen/server/models/healthRecord.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
 const healthRecordSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   weight: Number,
   height: Number,
   age: Number,
   gender: String,
   bmi: Number,
-  bmiCategory: String, 
+  bmiCategory: String,
   bmr: Number,
   tdee: Number,
   activityLevel: String,
@@ -15,6 +20,7 @@ const healthRecordSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
 
 const HealthRecord = mongoose.model("HealthRecord", healthRecordSchema);
 
