eaa06373bde6f87cb0720d5592b6bf1df5289c9b
aekasit <aekasit2002@gmail.com>
2025-05-11 02:04:41 +0700

    refactor: uncomment setting schema definition for improved readability

--- home-screen/server/models/setting.js
+++ home-screen/server/models/setting.js
@@ -1,15 +1,15 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const settingSchema = new mongoose.Schema(
-//   {
-//     userId: { type: String, required: true, unique: true },
-//     settings: {
-//       profileImage: String,
-//       currentPassword: String,
-//       passwordLastChanged: Date,
-//     },
-//   },
-//   { timestamps: true }
-// );
+const settingSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true, unique: true },
+    settings: {
+      profileImage: String,
+      currentPassword: String,
+      passwordLastChanged: Date,
+    },
+  },
+  { timestamps: true }
+);
 
-// export default mongoose.model("Setting", settingSchema);
+export default mongoose.model("Setting", settingSchema);
