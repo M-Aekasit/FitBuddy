9b4077de90a6414b1d94d703d4d9fabb1ac24a44
aekasit <aekasit2002@gmail.com>
2025-05-12 12:36:38 +0700

    Remove privacy and security pages, CSS, main entry point, utility functions, and configuration files

--- home-screen/server/routes/diaryRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createDiary, getDiary } from "../controllers/diaryController.js";
-
-const router = express.Router();
-
-router.post("/diary", createDiary);
-router.get("/diary", getDiary);
-
-export default router;
--- home-screen/server/routes/sportRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createSport, getSport } from "../controllers/sportController.js";
-
-const router = express.Router();
-router.post("/sport", createSport);
-router.get("/sport", getSport);
-
-export default router;
